2c1b871d79888bb8618bc2b893ef93453aca7a31
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 19:25:39 +0300

    Updated component 'InventoryItem' - Added store 'useInventoryStore' and updated styles

--- src/components/InventoryItem.vue
+++ src/components/InventoryItem.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script setup>
-import { useInventoryStore } from "@/store";
+import { useInventoryStore } from "@/stores/store";
 import { defineProps } from "vue";
 
 const props = defineProps({
@@ -31,3 +31,35 @@ const selectItem = () => {
   store.setSelectedItem(props.item);
 };
 </script>
+
+<style lang="scss">
+.inventory-item {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .item-background {
+    width: 80px;
+    height: 80px;
+    background-color: green;
+    opacity: 0.5;
+    border-radius: 15%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0 0 15px 3px rgba(255, 255, 255, 0.5);
+    transition: all 0.1s ease-in;
+  }
+  &:hover .item-background {
+    transform: scale(1.1);
+  }
+  .item-count {
+    color: white;
+    font-size: 1.2em;
+  }
+}
+</style>
