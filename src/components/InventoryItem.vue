c51fc6abc5053512806251589abb54dffe6a1760
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 20:59:09 +0300

    Updated component 'InventoryItem' - Updated styles

--- src/components/InventoryItem.vue
+++ src/components/InventoryItem.vue
@@ -33,13 +33,16 @@ const selectItem = () => {
 <style lang="scss">
 .inventory-item {
   position: absolute;
-  width: 48px;
-  height: 48px;
-  cursor: pointer;
+  top: 0;
+  left: 0;
+  width: 100px;
+  height: 100px;
   display: flex;
+  cursor: pointer;
   align-items: center;
   justify-content: center;
   z-index: 2;
+  transform: translate(50%, 50%);
   .item-background {
     width: 48px;
     height: 48px;
@@ -56,36 +59,36 @@ const selectItem = () => {
     transform: scale(1.1);
     box-shadow: 0 0 15px 6px rgba(255, 255, 255, 0.5);
   }
-  .item-count {
-    position: absolute;
-    font-family: var(--font-family);
-    font-weight: 500;
-    font-size: 10px;
-    text-align: center;
-    color: #fff;
-    opacity: 0.4;
-    bottom: 0;
-    right: 0;
-    padding-right: 3px;
-    padding-bottom: 2px;
-    &::before {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      right: 0;
-      border: 1px solid #4d4d4d;
-      border-radius: 6px 0 0 0;
-      width: 16px;
-      height: 16px;
-      background: #262626;
-    }
-  }
   &.stacked {
-    transform: translate(6px, -6px);
+    transform: translate(56px, 44px);
     .item-background {
       backdrop-filter: blur(12px);
       background: rgba(184, 217, 152, 0.35);
     }
+  }
+}
+.item-count {
+  position: absolute;
+  font-family: var(--font-family);
+  font-weight: 500;
+  font-size: 10px;
+  text-align: center;
+  color: #fff;
+  opacity: 0.4;
+  bottom: 0;
+  right: 0;
+  padding-right: 3px;
+  padding-bottom: 2px;
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    border: 1px solid #4d4d4d;
+    border-radius: 6px 0 0 0;
+    width: 16px;
+    height: 16px;
+    background: #262626;
   }
 }
 </style>
