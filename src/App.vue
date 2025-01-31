7813c946a8b71f2ef177b6f6722976826e437281
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 20:16:16 +0300

    Updated 'App' - Updated styles

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,25 @@ import AddItemBar from "./components/AddItemBar.vue";
 </script>
 
 <style lang="scss">
+:root {
+  --font-family: "Inter", sans-serif;
+  --second-family: "SF Pro Display", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: var(--font-family);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 10px;
 }
+
 body {
   background-color: black;
+  margin: 0;
 }
 </style>
