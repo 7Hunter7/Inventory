0050bdb06e59d0a38c34b24a06956d86039af632
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 19:29:31 +0300

    Added new component 'AddItemBar'

---
+++ src/components/AddItemBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="add-item-bar">
+    <input type="number" v-model.number="itemCount" placeholder="Кол-во" />
+    <button @click="addItem">Добавить предмет</button>
+  </div>
+</template>
+
+<script setup>
+import { useInventoryStore } from "@/stores/store";
+import { ref } from "vue";
+
+const store = useInventoryStore();
+const itemCount = ref(1);
+
+const addItem = () => {
+  store.addItem({ x: 0, y: 0, count: itemCount.value });
+};
+</script>
+
+<style lang="scss">
+.add-item-bar {
+  display: flex;
+  justify-content: center;
+  input {
+    margin-right: 10px;
+  }
+}
+</style>
