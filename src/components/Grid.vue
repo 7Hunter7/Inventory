4318344f8ac1637a164b31ff1d71a79b19809970
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 20:35:30 +0300

    Updated component 'Grid' - Added 'item-count' and computed 'itemsCount'

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="grid">
     <div class="grid-row" v-for="row in 5" :key="row">
-      <div class="grid-cell" v-for="col in 5" :key="col" ref="gridCells"></div>
+      <div
+        class="grid-cell"
+        v-for="(col, index) in 5"
+        :key="index"
+        ref="gridCells"
+      >
+        <span class="item-count" v-if="itemsCount[index] > 0">{{
+          itemsCount[index]
+        }}</span>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { useInventoryStore } from "@/stores/store";
+import { storeToRefs } from "pinia";
 
+const store = useInventoryStore();
+const { items } = storeToRefs(store);
 const gridCells = ref([]);
+const itemsCount = computed(() => {
+  const count = new Array(25).fill(0); // 5x5 grid
+  items.value.forEach((item) => {
+    const index = item.y * 5 + item.x;
+    count[index]++;
+  });
+  return count;
+});
 </script>
 
 <style lang="scss">
