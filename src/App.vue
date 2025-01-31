5d3cbda22ca1cfb5fd6c2b7a15ce1ef4ce39a80d
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 19:21:40 +0300

    Updated 'App'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <Inventory />
+    <ItemModal />
+    <AddItemBar />
+  </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+<script setup>
+import Inventory from "./components/Inventory.vue";
+import ItemModal from "./components/ItemModal.vue";
+import AddItemBar from "./components/AddItemBar.vue";
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  background-color: black;
 }
 </style>
