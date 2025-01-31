7f4beb49d6e6769a1cae46ffbe417920574a01c2
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 20:20:06 +0300

    Updated component 'Grid' - Updated styles

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -10,17 +10,23 @@
 .grid {
   display: flex;
   flex-direction: column;
-  border: 1px solid rgba(255, 255, 255, 0.1);
+  border: 1px solid #4d4d4d;
+  border-radius: 12px;
+  width: 525px;
+  height: 500px;
+  background: #262626;
   .grid-row {
     display: flex;
-    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+    border-bottom: 1px solid #4d4d4d;
+    width: 100%;
+    height: 100px;
     &:last-child {
       border-bottom: none;
     }
     .grid-cell {
-      width: 100px;
+      width: 105px;
       height: 100px;
-      border-right: 1px solid rgba(255, 255, 255, 0.1);
+      border-right: 1px solid #4d4d4d;
       &:last-child {
         border-right: none;
       }
