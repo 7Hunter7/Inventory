3006b8681955d2eb20eebd6dcd407e02fee026d5
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 19:23:03 +0300

    Added new component 'Inventory'

---
+++ src/components/Inventory.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="inventory">
+    <Grid />
+    <InventoryItem v-for="item in items" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script setup>
+import InventoryItem from "./InventoryItem.vue";
+import Grid from "./Grid.vue";
+import { useInventoryStore } from "@/stores/store";
+import { storeToRefs } from "pinia";
+
+const store = useInventoryStore();
+const { items } = storeToRefs(store);
+</script>
+
+<style lang="scss">
+.inventory {
+  position: relative;
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+}
+</style>
