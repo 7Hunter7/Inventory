85181b1d12e0b7f4701f043717be09823132a1f6
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-31 19:13:48 +0300

    Added new component 'InventoryItem.vue'

---
+++ src/components/InventoryItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="inventory-item"
+    :style="{
+      top: y * 100 + 'px',
+      left: x * 100 + 'px',
+    }"
+    @click="selectItem"
+  >
+    <div class="item-background"></div>
+    <span class="item-count">{{ item.count }}</span>
+  </div>
+</template>
+
+<script setup>
+import { useInventoryStore } from "@/store";
+import { defineProps } from "vue";
+
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+});
+
+const { x, y } = props.item;
+
+const store = useInventoryStore();
+const selectItem = () => {
+  store.openModal();
+  store.setSelectedItem(props.item);
+};
+</script>
