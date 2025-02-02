7988fdc7c6c5759a83cb454e6a4690ec24891f90
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-02 14:52:55 +0300

    Updated 'main.js' - Added import and usage 'persistedstate'

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 const pinia = createPinia();
-const app = createApp(App);
+pinia.use(piniaPluginPersistedstate);
 
+const app = createApp(App);
 app.use(pinia);
 app.mount("#app");
