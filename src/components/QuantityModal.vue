2a592f019a3f4642ed66ec0dab59f309c2921fee
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-02 15:40:38 +0300

    Updated component 'QuantityModal' - Updated template, updated styles, and  added import and usage 'useInventoryStore'

--- src/components/QuantityModal.vue
+++ src/components/QuantityModal.vue
@@ -1,88 +1,97 @@
 <template>
-  <div class="item-modal" v-if="isModalOpen">
-    <div class="item-modal-content">
-      <button @click="closeModal">X</button>
-      <div class="modal-item-wrapper">
-        <div class="modal-item-background"></div>
-        <div class="modal-item-count">Количество: {{ selectedItem.count }}</div>
+  <div class="modal-overlay" @click.self="closeModal">
+    <div class="modal" :style="modalPosition">
+      <div class="modal-content">
+        <label for="quantity">Введите количество:</label>
+        <input
+          type="number"
+          id="quantity"
+          v-model.number="quantityToRemove"
+          min="1"
+          :max="item?.quantity"
+        />
+        <div class="modal-buttons">
+          <button @click="closeModal">Отмена</button>
+          <button @click="removeItems">Удалить</button>
+        </div>
       </div>
-
-      <button @click="removeItem(selectedItem.id)">Удалить предмет</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { useInventoryStore } from "@/stores/store";
-import { storeToRefs } from "pinia";
+import { useInventoryStore } from "../store";
+import { ref } from "vue";
 
-const store = useInventoryStore();
-const { isModalOpen, selectedItem } = storeToRefs(store);
+const quantityToRemove = ref(1);
+const inventoryStore = useInventoryStore();
+
+const props = defineProps({
+  item: Object,
+});
+
 const closeModal = () => {
-  store.closeModal();
+  inventoryStore.closeQuantityModal();
 };
-const removeItem = (id) => {
-  store.removeItem(id);
-  store.closeModal();
+
+const removeItems = () => {
+  inventoryStore.removeItem(props.item.id, quantityToRemove.value);
+  closeModal();
+  inventoryStore.closeItemModal();
+};
+
+const modalPosition = () => {
+  return {
+    right: 0,
+    bottom: 0,
+    transform: "translate(0, 100%)",
+  };
 };
 </script>
 
-<style lang="scss" scoped>
-.item-modal {
+<style scoped>
+.modal-overlay {
   position: fixed;
-  bottom: 0;
+  top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.5);
   display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 100;
-  .item-modal-content {
-    background-color: #333;
-    padding: 20px;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    button {
-      margin-bottom: 10px;
-      border: none;
-      background-color: #747474;
-      color: #fff;
-      padding: 5px;
-      border-radius: 3px;
-      cursor: pointer;
-      transition: all 0.1s ease-in;
-      &:hover {
-        transform: scale(1.1);
-        background-color: #a5a5a5;
-      }
-      &:first-child {
-        align-self: flex-end;
-        background-color: #952525;
-        &:hover {
-          background-color: #b62e2e;
-        }
-      }
-    }
-    .modal-item-wrapper {
-      display: flex;
-      .modal-item-background {
-        width: 48px;
-        height: 48px;
-        background-color: #7faa65;
-        opacity: 0.5;
-        border-radius: 15%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-right: 10px;
-      }
-      .modal-item-count {
-        font-size: 1.2em;
-      }
-    }
-  }
+  justify-content: flex-end;
+  align-items: flex-end;
+  z-index: 20;
+}
+.modal {
+  position: absolute;
+  background: #333;
+  padding: 20px;
+  border-radius: 12px;
+  width: 300px;
+}
+.modal-content {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: #fff;
+}
+.modal-buttons {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 10px;
+  gap: 10px;
+}
+.modal-buttons button {
+  padding: 10px 20px;
+  border: none;
+  background-color: #515151;
+  color: #fff;
+  cursor: pointer;
+}
+input[type="number"] {
+  padding: 8px;
+  border: none;
+  background-color: #262626;
+  color: #fff;
 }
 </style>
