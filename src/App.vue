94e7ade99e711354940f79ff2b38a3fabdc684a8
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-03 16:19:35 +0300

    Updated 'App' - Added mathod 'handleEscClose'

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import SidebarComponent from "./components/SidebarComponent.vue";
 import InventoryGrid from "./components/InventoryGrid.vue";
 import ItemModal from "./components/ItemModal.vue";
@@ -26,6 +26,25 @@ import { useInventoryStore } from "./stores/store.js";
 
 const inventoryStore = useInventoryStore();
 const inventoryGridRef = ref(null);
+
+const handleEscClose = (event) => {
+  if (event.key === "Escape") {
+    if (inventoryStore.isQuantityModalOpen) {
+      inventoryStore.closeQuantityModal();
+    } else if (inventoryStore.isItemModalOpen) {
+      inventoryStore.closeItemModal();
+    }
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("keydown", handleEscClose);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", handleEscClose);
+});
+
 function handleFooterClose() {
   // Логика при нажатии на кнопку закрытия в подвале
   console.log("Footer closed");
