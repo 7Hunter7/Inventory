f1f80251c0ba0dc92013bc9db8c4f87f3c80125e
Hunter <i.am.hunter.777.ih@gmail.com>
2025-02-06 06:54:38 +0300

    Updated component 'DeleteButton' to 'ButtonWithText'

---
+++ src/components/ButtonWithText.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="button delete-button" @click="$emit('click')">
+    <slot v-if="text">{{ text }}</slot>
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    default: null,
+  },
+});
+
+const emit = defineEmits(["click"]);
+</script>
+
+<style lang="scss" scoped>
+.delete-button {
+  width: 100%;
+  padding: 0.6875rem;
+  color: #fff;
+  background-color: #fa7272;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #f88;
+  }
+  &:active {
+    background-color: #fa7272;
+  }
+}
+</style>
